a84a7f8f4ce33ba031c8e5107928dc4c9a4386c5
Timothy Sherry <timsherry97@gmail.com>
2019-03-03 22:15:34 -0500

    Initial Commit

---
+++ CMakeFiles/a2.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/a2.dir/a2.o"
+  "a2.pdb"
+  "a2"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/a2.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ a2.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <opencv2/opencv.hpp>
+
+using namespace std;
+using namespace cv;
+
+int main(int argc, char** argv ) {
+  if(argc != 2) {
+    cout << "usage: DisplayImage.out <Image_Path>\n";
+    return -1;
+  }
+
+  Mat image;
+  image = imread(argv[1], 1);
+
+  if(!image.data) {
+    cout << "No image data \n";
+    return -1;
+  }
+
+  namedWindow("Display Image", WINDOW_AUTOSIZE);
+  imshow("Display Image", image);
+
+  waitKey(0);
+
+  return 0;
+}
